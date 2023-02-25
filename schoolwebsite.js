54417a3a72591642e802a42e6876d333c03a86c8
Vincent <108121079+Vin4eva@users.noreply.github.com>
2023-02-25 09:18:32 +0100

    Add files via upload
    still under design

---
+++ schoolwebsite.js
@@ -0,0 +1,14 @@
+const navLinks = document.querySelector("#navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-300px";
+} 
+
+/* const form = document.querySelector("form");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+}) */
